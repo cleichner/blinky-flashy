b17f3b75ad73d9702fa9f0ee3851999fb65d44bf
Chas Leichner <chas@chas.io>
2014-01-19 22:30:23 -0700

    Checkerboard pattern working

--- src/sketch.ino
+++ src/sketch.ino
@@ -8,13 +8,14 @@ void setup() {
     initialize_image(image);
 }
 
-void initialize_image(image) {
+void initialize_image(uint16_t image[NUM_ROWS][NUM_COLS]) {
     int init = 0;
     for (uint8_t i = 0; i < NUM_ROWS; ++i) {
         for (uint8_t j = 0; j < NUM_COLS; ++j) {
             image[i][j] = init & 0b1;
             init++;
         }
+        init++;
     }
 }
 
@@ -34,16 +35,27 @@ void alternate(uint16_t image[NUM_ROWS][NUM_COLS], uint16_t scale) {
     }
 }
 
+uint16_t wait = 0;
+
 void loop()
 {
     initialize_image(image);
     brighten(image, scale);
+
+    if (wait < 200) {
+        alternate(image, scale);
+    }
+
     Display.show(image);
-    alternate(image, scale);
+
+    wait++;
+    if (wait > 400) {
+        wait = 0;
+    }
 
     scale++;
     if (scale > 4000) {
         scale = 0;
     }
-    delay(33);
+//    delay(33);
 }
