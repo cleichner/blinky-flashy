5385cb5a9132db6ed050e2683c94b0a9c1c79a89
Chas Leichner <chas@chas.io>
2014-01-19 22:23:49 -0700

    Removed Timer3

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,37 +1,49 @@
 #include "Display.h"
 
+uint16_t scale = 0;
+uint16_t image[NUM_ROWS][NUM_COLS];
+
 void setup() {
     Display.init();
+    initialize_image(image);
 }
 
-uint16_t image[NUM_ROWS][NUM_COLS] = {
-    {0, 2000, 0, 2000, 0, 2000, 0, 2000},
-    {2000, 0, 2000, 0, 2000, 0, 2000, 0},
-    {0, 2000, 0, 2000, 0, 2000, 0, 2000},
-    {2000, 0, 2000, 0, 2000, 0, 2000, 0},
-    {0, 2000, 0, 2000, 0, 2000, 0, 2000},
-    {2000, 0, 2000, 0, 2000, 0, 2000, 0},
-    {0, 2000, 0, 2000, 0, 2000, 0, 2000},
-    {2000, 0, 2000, 0, 2000, 0, 2000, 0},
-    {0, 2000, 0, 2000, 0, 2000, 0, 2000},
-    {2000, 0, 2000, 0, 2000, 0, 2000, 0},
-    {0, 2000, 0, 2000, 0, 2000, 0, 2000},
-    {2000, 0, 2000, 0, 2000, 0, 2000, 0},
-};
-
-
-void alternate(uint16_t image[NUM_ROWS][NUM_COLS]) {
+void initialize_image(image) {
+    int init = 0;
     for (uint8_t i = 0; i < NUM_ROWS; ++i) {
         for (uint8_t j = 0; j < NUM_COLS; ++j) {
-            image[i][j] = 2000 - image[i][j];
+            image[i][j] = init & 0b1;
+            init++;
+        }
+    }
+}
+
+void brighten(uint16_t image[NUM_ROWS][NUM_COLS], uint16_t scale) {
+    for (uint8_t i = 0; i < NUM_ROWS; ++i) {
+        for (uint8_t j = 0; j < NUM_COLS; ++j) {
+            image[i][j] *= scale;
+        }
+    }
+}
+
+void alternate(uint16_t image[NUM_ROWS][NUM_COLS], uint16_t scale) {
+    for (uint8_t i = 0; i < NUM_ROWS; ++i) {
+        for (uint8_t j = 0; j < NUM_COLS; ++j) {
+            image[i][j] = scale - image[i][j];
         }
     }
 }
 
 void loop()
 {
+    initialize_image(image);
+    brighten(image, scale);
     Display.show(image);
-    alternate(image);
+    alternate(image, scale);
 
-    delay(100);
+    scale++;
+    if (scale > 4000) {
+        scale = 0;
+    }
+    delay(33);
 }
