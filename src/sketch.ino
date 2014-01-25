0ecac9ec5e094282bb76c9a0505507b0c517f758
Chas Leichner <chas@chas.io>
2014-01-25 11:13:31 -0700

    Carcomms working

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,45 +1,107 @@
 #include "Display.h"
+#include "Timer.h"
+
+#define UPDATE_PERIOD 2000
+// every, every (with repeat), after,
 
 uint16_t intensity = 0;
 
-void setup() {
-    Display.init();
+bool vertical_up = true;
+uint8_t vertical_length = 0;
+
+bool horizontal_up = true;
+uint8_t horizontal_length = 1;
+
+Timer t;
+
+void update_state() {
+    Serial.println("STATE");
+    horizontal_length++;
+    if (horizontal_length & 0x1) {
+        vertical_length++;
+    }
+
+    if (horizontal_length > NUM_COLS) {
+        horizontal_length = 1;
+    }
+
+    if (vertical_length > NUM_ROWS) {
+        vertical_length = 0;
+    }
+
+    if (horizontal_length == 1 && vertical_length == 0) {
+        intensity = 0;
+    }
 }
 
+void brighten() {
+    Serial.print("*");
+    if (intensity < 4095) {
+        intensity++;
+    } else {
+        intensity = 0;
+    }
+    Display.show();
+}
+
+int forwardInput = 5;
+int backwardInput = 6;
+int leftInput = 7;
+int rightInput = 8;
+
+bool goingForward() {
+    return !digitalRead(forwardInput);
+}
+
+bool goingBackward() {
+    return !digitalRead(backwardInput);
+}
+
+bool goingLeft() {
+    return !digitalRead(leftInput);
+}
+
+bool goingRight() {
+    return !digitalRead(rightInput);
+}
+
+void setup() {
+    Serial.begin(9600);
+    Display.init();
+    pinMode(forwardInput, INPUT);      // sets the digital pin as output
+    pinMode(backwardInput, INPUT);      // sets the digital pin as output
+    pinMode(leftInput, INPUT);      // sets the digital pin as output
+    pinMode(rightInput, INPUT);      // sets the digital pin as output
+
+    intensity = 2000;
+
+
+//    t.every(1000, update_state);
+//    t.every(50, brighten);
+}
 
 void loop()
 {
-    static uint8_t vertical_length = 0;
-    static uint8_t horizontal_length = 1;
-    static bool offset = false;
-    static uint16_t wait = 0;
+//    Serial.print(".");
+    Serial.print("forward ");
+    Serial.print(goingForward());
+    Serial.print(" backward ");
+    Serial.print(goingBackward());
+    Serial.print(" left ");
+    Serial.print(goingLeft());
+    Serial.print(" right ");
+    Serial.println(goingRight());
+    delay(250);
 
-    Display.clear();
+//    Display.clear();
+ //   if (digitalRead(going_forward)) {
+ //       Display.line(8, 0, 8, HORIZONTAL, 500);
+ //   }
 
-    Display.line(1 + vertical_length, 0, 2, VERTICAL, intensity/7);
-    Display.line(0, horizontal_length, 2, HORIZONTAL, intensity/7);
-    Display.line(1, 0, vertical_length, VERTICAL, intensity);
-    Display.line(0, 0, horizontal_length, HORIZONTAL, intensity);
+//    Display.line(1 + vertical_length, 0, 2, VERTICAL, intensity/7);
+//    Display.line(0, horizontal_length, 2, HORIZONTAL, intensity/7);
+//    Display.line(1, 0, vertical_length, VERTICAL, intensity);
+//    Display.line(0, 0, horizontal_length, HORIZONTAL, intensity);
 
-    Display.show();
-
-    if (++wait >= 400) {
-        offset = !offset;
-        wait = 0;
-        horizontal_length++;
-        if (horizontal_length & 0x1) {
-            vertical_length++;
-        }
-
-        if (horizontal_length > NUM_COLS) {
-            horizontal_length = 0;
-        }
-        if (vertical_length > NUM_ROWS) {
-            vertical_length = 0;
-        }
-    }
-
-    if (++intensity >= 4095) {
-        intensity = 0;
-    }
+    t.update();
 }
